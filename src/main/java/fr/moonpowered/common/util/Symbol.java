0601312bff5a15070a756be344e51611187190cd
kurai <67955616+kuraiwtf@users.noreply.github.com>
2025-09-29 21:21:24 +0200

    Add symbols constants to Symbol utility class

--- src/main/java/fr/moonpowered/common/util/Symbol.java
+++ src/main/java/fr/moonpowered/common/util/Symbol.java
@@ -2,5 +2,64 @@ package fr.moonpowered.common.util;
 
 public final class Symbol {
 
+  public static final String BAR = "❘";
+  public static final String BAR_2 = "▎";
+  public static final String BAR_3 = "▍";
+  public static final String SQUARE = "▪";
+  public static final String SQUARE_EMPTY = "▫";
+  public static final String BIG_SQUARE = "■";
+  public static final String DANGER = "⚠";
+  public static final String HEART = "❤";
+  public static final String INFO = "ⓘ";
+  public static final String ARROW_RIGHT = "➽";
+  public static final String BURGER = "☰";
+  public static final String PEN = "✎";
+  public static final String BERRY = "❦";
+  public static final String COINS = "⛃";
+  public static final String CHECKMARK = "✔";
+  public static final String CROSS = "✖";
+  public static final String INFINITE = "∞";
+  public static final String CURVED_ARROW_RIGHT = "➥";
+  public static final String BIG_ARROW_RIGHT = "➲";
+
+  public static final String SUN = "✴";
+  public static final String BIG_SUN = "✹";
+
+  public static final String STAR = "✰";
+  public static final String BIG_STAR = "✪";
+
+  public static final String FLOWER = "✿";
+
+  public static final String DIAMOND = "✦";
+  public static final String DIAMOND_2 = "◊";
+
+  public static final String CIRCLE = "•";
+
+  public static final String YIN_YANG = "☯";
+
+  public static final String DIRECTIONAL_CROSS = "❖";
+
+  public static final String ONE_CIRCLE_EMPTY = "①";
+  public static final String TWO_CIRCLE_EMPTY = "②";
+  public static final String THREE_CIRCLE_EMPTY = "③";
+  public static final String FOUR_CIRCLE_EMPTY = "④";
+  public static final String FIVE_CIRCLE_EMPTY = "⑤";
+  public static final String SIX_CIRCLE_EMPTY = "⑥";
+  public static final String SEVEN_CIRCLE_EMPTY = "⑦";
+  public static final String EIGHT_CIRCLE_EMPTY = "⑧";
+  public static final String NINE_CIRCLE_EMPTY = "⑨";
+  public static final String TEN_CIRCLE_EMPTY = "⑩";
+
+  public static final String ONE_CIRCLE_FILL = "❶";
+  public static final String TWO_CIRCLE_FILL = "❷";
+  public static final String THREE_CIRCLE_FILL = "❸";
+  public static final String FOUR_CIRCLE_FILL = "❹";
+  public static final String FIVE_CIRCLE_FILL = "❺";
+  public static final String SIX_CIRCLE_FILL = "❻";
+  public static final String SEVEN_CIRCLE_FILL = "❼";
+  public static final String EIGHT_CIRCLE_FILL = "❽";
+  public static final String NINE_CIRCLE_FILL = "❾";
+  public static final String TEN_CIRCLE_FILL = "❿";
+
   private Symbol() {}
 }
