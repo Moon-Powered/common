dc4f009b3770882708a65950f1f08fcecdfe89c4
kurai <67955616+kuraiwtf@users.noreply.github.com>
2025-10-16 23:02:30 +0200

    chore: remove redundant OSSRH repository configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -66,17 +66,6 @@ publishing {
             }
         }
     }
-
-    repositories {
-        maven {
-            name = "OSSRH"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = System.getenv("OSSRH_USERNAME")
-                password = System.getenv("OSSRH_PASSWORD")
-            }
-        }
-    }
 }
 
 nexusPublishing {
