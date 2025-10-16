67974a646b6869c19343ea7304961757720ac6ed
kurai <67955616+kuraiwtf@users.noreply.github.com>
2025-10-16 23:13:54 +0200

    chore: migrate from Maven Central to GitHub Packages for artifact publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     `java-library`
     `maven-publish`
-    signing
-    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
 }
 
 group = "fr.moonpowered.common"
@@ -37,50 +35,17 @@ publishing {
             groupId = project.group.toString()
             artifactId = project.name
             version = project.version.toString()
+        }
+    }
 
-            pom {
-                name.set("Common")
-                description.set("A library that provides common utilities and abstractions for Java applications.")
-                url.set("https://github.com/Moon-Powered/common")
-
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/license/mit/")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("kurai")
-                        name.set("kurai")
-                        email.set("unchatongo@gmail.com")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/Moon-Powered/common.git")
-                    developerConnection.set("scm:git:ssh://github.com/Moon-Powered/common.git")
-                    url.set("https://github.com/Moon-Powered/common")
-                }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/Moon-Powered/common")
+            credentials {
+                username = System.getenv("githubActor")
+                password = System.getenv("githubPassword")
             }
         }
     }
-}
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            username.set(System.getenv("OSSRH_USERNAME"))
-            password.set(System.getenv("OSSRH_PASSWORD"))
-        }
-    }
-}
-
-signing {
-    useInMemoryPgpKeys(
-        System.getenv("GPG_PRIVATE_KEY"),
-        System.getenv("GPG_PASSPHRASE")
-    )
-    sign(publishing.publications["common"])
 }
